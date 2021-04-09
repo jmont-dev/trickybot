4aad244c54197acbe2814c454492117922a95fcc
James Montgomery <James.Brian.Montgomery@gmail.com>
2021-04-09 00:17:05 -0400

    Update trickybot.py

--- trickybot.py
+++ trickybot.py
@@ -284,6 +284,13 @@ async def dongs(ctx) :
     await ctx.send(f"{random.choice(memes)}")
 
 @client.command()
+async def goodshit(ctx) :     
+
+    text = "👌👀👌👀👌👀👌👀👌👀 good shit go౦ԁ sHit👌 thats ✔ some good👌👌shit right👌👌there👌👌👌 right✔there ✔✔if i do ƽaү so my self 💯 i say so 💯 thats what im talking about right there right there (chorus: ʳᶦᵍʰᵗ ᵗʰᵉʳᵉ) mMMMMᎷМ💯 👌👌 👌НO0ОଠOOOOOОଠଠOoooᵒᵒᵒᵒᵒᵒᵒᵒᵒ👌 👌👌 👌 💯 👌 👀 👀 👀 👌👌Good shit"
+
+    await ctx.send(f"{text}")
+
+@client.command()
 async def vibe(ctx) :
 
     cats = [f"https://www.youtube.com/watch?v=eZTS4cL4Euo",
