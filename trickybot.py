394b1d55aafdda903bac356e62299c332f725c57
James Montgomery <James.Brian.Montgomery@gmail.com>
2021-05-06 14:06:26 -0400

    Add untrain function.

--- trickybot.py
+++ trickybot.py
@@ -109,6 +109,13 @@ question_answering = pipeline("question-answering")
 
 qa_knowledge = "Machine learning (ML) is the study of computer algorithms that improve automatically through experience. It is seen as a part of artificial intelligence. Machine learning algorithms build a model based on sample data, known as training data, in order to make predictions or decisions without being explicitly programmed to do so. Machine learning algorithms are used in a wide variety of applications, such as email filtering and computer vision, where it is difficult or unfeasible to develop conventional algorithms to perform the needed tasks."
 
+@client.command(aliases=['u'])
+async def untrain(ctx, *args) :
+
+    global qa_knowledge
+    qa_knowledge = ""
+
+    await ctx.send(f"Removed all training data.")
 
 @client.command(aliases=['e'])
 async def educate(ctx, *args) :
